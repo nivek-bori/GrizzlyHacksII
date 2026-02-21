1c4220d5a6d379a18f87d727503e40d517e7ae6c
AnshulM27 <149454763+AnshulM27@users.noreply.github.com>
2026-02-21 12:24:10 -0800

    Update HomeComponent.tsx
    Makes canvas, draws image onto canvas, draws bounding boxes, draws maximally sized OpenDyslexic text onto bounding boxes.

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -37,33 +37,23 @@ export default function HomeComponent() {
         canvas.height = img.height;
         const ctx = canvas.getContext('2d');
         if (ctx) ctx.drawImage(img, 0, 0);
+        for (const text of testingData) {
+          ctx.fillStyle = "white";
+          ctx.fillRect(text.bounding_box.x1, text.bounding_box.y1, text.bounding_box.x1-text.bounding_box.x0, text.bounding_box.y1-text.bounding_box.y0);
+
+          let fontSize = 12;
+          ctx.font = `${fontSize}px OpenDyslexic`;
+          const metrics = ctx.measureText(text.text);
+          const ratio = Math.min((text.bounding_box.x1 - text.bounding_box.x0) / metrics.width, (text.bounding_box.y1 - text.bounding_box.y0) / fontSize);
+          fontSize = Math.floor(ratio * fontSize);
+
+          ctx.fillStyle = "black";
+          ctx.font = `${fontSize}px OpenDyslexic`;
+          ctx.fillText(text.text, text.bounding_box.x0 + (text.bounding_box.x1 - text.bounding_box.x0) / 2 - ctx.measureText(text.text).width / 2, text.bounding_box.y0 + (text.bounding_box.y1 - text.bounding_box.y0) / 2 - fontSize / 2);
+        }
       };
       img.src = imageData;
     }
-
-    for (const text of testingData) {
-      
-      // draw rectangel on canvas
-      // write text
-
-      ctx.fillStyle = "white";
-      ctx.fillRect = (text.bounding_box.x1, text.bounding_box.y1, text.bounding_box.x1-text.bounding_box.x0, text.bounding_box.y1-text.bounding_box.y0);
-
-      let fontSize = 12;
-      ctx.font = `${fontSize}px OpenDyslexic`;
-      const metrics = ctx.measureText(text.text);
-      const ratio = Math.min((text.bounding_box.x1 - text.bounding_box.x0) / metrics.width, (text.bounding_box.y1 - text.bounding_box.y0) / fontSize);
-      fontSize = Math.floor(ratio * fontSize);
-
-      ctx.fillStyle = "black";
-      ctx.font = `${fontSize}px OpenDyslexic`;
-      ctx.fillText(text.text, text.bounding_box.x0 + (text.bounding_box.x1 - text.bounding_box.x0) / 2 - ctx.measureText(text.text) / 2, text.bounding_box.y0 + (text.bounding_box.y1 - text.bounding_box.y0) / 2 - fontSize / 2);
-      
-
-    }
-
-    
-
 
   }
 
